4badbcec2a9d37daf424b96a76a0e5b9ce026127
Arul <43119840+ArulHacks@users.noreply.github.com>
2018-10-27 08:50:44 -0700

    Mecanum Working Code :)
    This is the code... The left joystick is all about lateral, diagonal, and general motion, and the right joystick is for rotation.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumPrototype.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumPrototype.java
@@ -1,35 +1,7 @@
-/* Copyright (c) 2017 FIRST. All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without modification,
- * are permitted (subject to the limitations in the disclaimer below) provided that
- * the following conditions are met:
- *
- * Redistributions of source code must retain the above copyright notice, this list
- * of conditions and the following disclaimer.
- *
- * Redistributions in binary form must reproduce the above copyright notice, this
- * list of conditions and the following disclaimer in the documentation and/or
- * other materials provided with the distribution.
- *
- * Neither the name of FIRST nor the names of its contributors may be used to endorse or
- * promote products derived from this software without specific prior written permission.
- *
- * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
- * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
- * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
- * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
-
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.hardware.DigitalChannel;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
@@ -37,23 +9,12 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.util.Range;
 
 
-/**
- * This file contains an minimal example of a Linear "OpMode". An OpMode is a 'program' that runs in either
- * the autonomous or the teleop period of an FTC match. The names of OpModes appear on the menu
- * of the FTC Driver Station. When an selection is made from the menu, the corresponding OpMode
- * class is instantiated on the Robot Controller and executed.
- *
- * This particular OpMode just executes a basic Tank Drive Teleop for a two wheeled robot
- * It includes all the skeletal structure that all linear OpModes contain.
- *
- * Use Android Studios to Copy this Class, and Paste it into your team's code folder with a new name.
- * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
- */
-
-@TeleOp(name="Mecanum Prototype", group="deCoders Prototypes")
+@TeleOp(name="Mecanum Type", group="Linear Opmode")
+//@Disabled
 public class MecanumPrototype extends LinearOpMode {
 
-    // Declare OpMode members.
+
+
     private ElapsedTime runtime = new ElapsedTime();
     private DcMotor LeftDriveFront = null;
     private DcMotor RightDriveFront = null;
@@ -65,94 +26,55 @@ public class MecanumPrototype extends LinearOpMode {
         telemetry.addData("Status", "Initialized");
         telemetry.update();
 
-        // Initialize the hardware variables. Note that the strings used here as parameters
-        // to 'get' must correspond to the names assigned during the robot configuration
-        // step (using the FTC Robot Controller app on the phone).
         LeftDriveFront  = hardwareMap.get(DcMotor.class, "LeftDriveFront");
         RightDriveFront = hardwareMap.get(DcMotor.class, "RightDriveFront");
         LeftDriveBack  = hardwareMap.get(DcMotor.class, "LeftDriveBack");
         RightDriveBack = hardwareMap.get(DcMotor.class, "RightDriveBack");
 
-
-        // Most robots need the motor on one side to be reversed to drive forward
-        // Reverse the motor that runs backwards when connected directly to the battery
         LeftDriveFront.setDirection(DcMotor.Direction.FORWARD);
         RightDriveFront.setDirection(DcMotor.Direction.REVERSE);
         LeftDriveBack.setDirection(DcMotor.Direction.FORWARD);
         RightDriveBack.setDirection(DcMotor.Direction.REVERSE);
 
-        // Wait for the game to start (driver presses PLAY)
         waitForStart();
         runtime.reset();
 
-        // run until the end of the match (driver presses STOP)
         while (opModeIsActive()) {
-
-            // Setup a variable for each drive wheel to save power level for telemetry
 
             double LeftFrontPower;
             double RightFrontPower;
             double RightBackPower;
             double LeftBackPower;
 
-            LeftFrontPower=1;
-            RightFrontPower=1;
-            RightBackPower=1;
-            LeftBackPower =1;
-            //double slidePower;
+            double drive = gamepad1.left_stick_y;
+            double slide = gamepad1.left_stick_x;
+            double turn = -gamepad1.right_stick_x;
 
 
-            // Choose to drive using either Tank Mode, or POV Mode
-            // Comment out the method that's not used.  The default below is POV.
+            LeftFrontPower = drive + slide + turn;
+            RightFrontPower = drive - slide - turn;
+            LeftBackPower = drive - slide + turn;
+            RightBackPower = drive + slide - turn;
 
-            // POV Mode uses left stick to go forward, and right stick to turn.
-            // - This uses basic math to combine motions and is easier to drive straight.
+            if(turn != 0 && (drive != 0 || slide != 0)){
+                LeftDriveFront.setPower(LeftFrontPower/2);
+                LeftDriveBack.setPower(LeftBackPower/2);
+                RightDriveFront.setPower(RightFrontPower/2);
+                RightDriveBack.setPower(RightBackPower/2);
 
-            //slidePower = -gamepad1.left_stick_x;
-            // Tank Mode uses one stick to control each wheel.
-            // - This requires no math, but it is hard to drive forward slowly and keep straight.
-            //  leftPower  = -gamepad1.left_stick_y ;
-            //  rightPower = -gamepad1.left_stick_y ;
-
-
-            // Send calculated power to wheels
-            if(gamepad1.y){
+            }
+            else{
                 LeftDriveFront.setPower(LeftFrontPower);
-                RightDriveFront.setPower(RightFrontPower);
                 LeftDriveBack.setPower(LeftBackPower);
+                RightDriveFront.setPower(RightFrontPower);
                 RightDriveBack.setPower(RightBackPower);
             }
-
-            else if(gamepad1.a){
-                LeftDriveFront.setPower(-LeftFrontPower);
-                RightDriveFront.setPower(-RightFrontPower);
-                LeftDriveBack.setPower(-LeftBackPower);
-                RightDriveBack.setPower(-RightBackPower);
-            }
-
-            else if(gamepad1.x){
-                LeftDriveFront.setPower(-LeftFrontPower);
-                RightDriveFront.setPower(-RightFrontPower);
-                LeftDriveBack.setPower(-LeftBackPower);
-                RightDriveBack.setPower(-RightBackPower);
-            }
-
-
-            else{
-                LeftDriveFront.setPower(0);
-                RightDriveFront.setPower(0);
-                LeftDriveBack.setPower(0);
-                RightDriveBack.setPower(0);
-            }
-
         }
 
-        // Show the elapsed game time and wheel power.
         telemetry.addData("Status", "Run Time: " + runtime.toString());
-
+        telemetry.addData("Motors","Left Front Power: (%.2f), Right Front Power: (%.2f), Left Back Power: (%.2f), Right Back Power: (%.2f)", LeftDriveFront.getPower(), RightDriveFront.getPower(), LeftDriveBack.getPower(), RightDriveBack.getPower());
+        telemetry.addData("Written by", "Arul and Samarth :)");
         telemetry.update();
 
-        idle();
     }
 }
-
