b2037dd04dee95663b286a56a08728bba23d2905
Sharad <sharad.pro007@gmail.com>
2018-10-27 18:10:05 -0700

    Uploading autonomous code

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousHookDescent.java
@@ -0,0 +1,167 @@
+/* Copyright (c) 2017 FIRST. All rights reserved.
+
+ * Written by deCoders Robotics Team
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted (subject to the limitations in the disclaimer below) provided that
+ * the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this list
+ * of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice, this
+ * list of conditions and the following disclaimer in the documentation and/or
+ * other materials provided with the distribution.
+ *
+ * Neither the name of FIRST nor the names of its contributors may be used to endorse or
+ * promote products derived from this software without specific prior written permission.
+ *
+ * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
+ * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.util.Range;
+import com.qualcomm.robotcore.hardware.DistanceSensor;
+
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+
+import java.util.concurrent.TimeUnit;
+
+
+/**
+ * This file contains an minimal example of a Linear "OpMode". An OpMode is a 'program' that runs in either
+ * the autonomous or the teleop period of an FTC match. The names of OpModes appear on the menu
+ * of the FTC Driver Station. When an selection is made from the menu, the corresponding OpMode
+ * class is instantiated on the Robot Controller and executed.
+ *
+ * This particular OpMode just executes a basic Tank Drive Teleop for a two wheeled robot
+ * It includes all the skeletal structure that all linear OpModes contain.
+ *
+ * Use Android Studios to Copy this Class, and Paste it into your team's code folder with a new name.
+ * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
+ */
+
+
+
+@Autonomous(name="Hook Descend Code", group="Linear Opmode")
+//@Disabled
+public class AutonomousCode extends LinearOpMode {
+
+    // Declare OpMode members.
+    private ElapsedTime runtime = new ElapsedTime();
+    private DcMotor lift = null;
+    private DcMotor LeftDriveFront = null;
+    private DcMotor RightDriveFront = null;
+    private DcMotor LeftDriveBack = null;
+    private DcMotor RightDriveBack = null;
+
+
+    DistanceSensor sensorRange;
+
+    @Override
+    public void runOpMode() {
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+
+        lift = hardwareMap.get(DcMotor.class, "lift");
+        sensorRange = hardwareMap.get(DistanceSensor.class, "range");
+
+        LeftDriveFront = hardwareMap.get(DcMotor.class, "LeftDriveFront");
+        RightDriveFront = hardwareMap.get(DcMotor.class, "RightDriveFront");
+        LeftDriveBack = hardwareMap.get(DcMotor.class, "LeftDriveBack");
+        RightDriveBack = hardwareMap.get(DcMotor.class, "RightDriveBack");
+
+        lift.setDirection(DcMotor.Direction.FORWARD);
+
+        LeftDriveFront.setDirection(DcMotor.Direction.FORWARD);
+        RightDriveFront.setDirection(DcMotor.Direction.REVERSE);
+        LeftDriveBack.setDirection(DcMotor.Direction.FORWARD);
+        RightDriveBack.setDirection(DcMotor.Direction.REVERSE);
+
+        lift.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+
+        // Wait for the game to start (driver presses PLAY)
+        waitForStart();
+        runtime.reset();
+
+        // run until the end of the match (driver presses STOP)
+        while (opModeIsActive()) {
+
+            while (sensorRange.getDistance(DistanceUnit.INCH) > 6){
+                lift.setPower(1.0);
+                //descending robot
+            }
+            while (sensorRange.getDistance(DistanceUnit.INCH) <= 6.1){
+                lift.setPower(1.0);
+                //raising arm to right position for unlatching
+            }
+
+            for (long stop=System.nanoTime()+TimeUnit.SECONDS.toNanos(3);stop>System.nanoTime();) {
+                LeftDriveFront.setPower(1.0);
+                RightDriveFront.setPower(-1.0);
+                LeftDriveBack.setPower(-1.0);
+                RightDriveBack.setPower(1.0);
+                //uses the mecanum wheels to move robot sideways and out of the hook for 3 seconds
+            }
+            while (sensorRange.getDistance(DistanceUnit.INCH) > 6){
+                lift.setPower(-1.0);
+                //shrinks the arm back down
+            }
+
+            for (long stop=System.nanoTime()+TimeUnit.SECONDS.toNanos(5);stop>System.nanoTime();) {
+                LeftDriveFront.setPower(-1.0);
+                RightDriveFront.setPower(-1.0);
+                LeftDriveBack.setPower(-1.0);
+                RightDriveBack.setPower(-1.0);
+                //reverses the robot and backes it away from the tower for 5 seconds
+            }
+
+//            if (sensorRange.getDistance(DistanceUnit.INCH) < 3.0){
+//                //if the sensor is less than 3 inches of the ground this code will trigger
+//                lift.setPower(0.0);
+//                //stop the arm from moving any more and lock it in place
+//                LeftDriveFront.setPower(1.0);
+//                RightDriveFront.setPower(-1.0);
+//                LeftDriveBack.setPower(1.0);
+//                RightDriveBack.setPower(-1.0);
+//                //needs to be accomplished: figure out how to make multiple actions happen at the same time for a set time
+//                //make motors turn and bring the hook out of the tower by turning sideways
+//            }
+//
+//            else{
+//                //otherwise run normal code
+//                lift.setPower(1.0);
+//                Thread.sleep(9000);
+//                //robot is descending and will hit the ground
+//                lift.setPower(-1.0);
+//                //shrink the hook arm back down so that it is compact
+//
+//
+//            }
+
+            // Show the elapsed game time and wheel power.
+            telemetry.addData("Status", "Run Time: " + runtime.toString());
+            telemetry.addData("Distance from Ground", sensorRange.getDistance(DistanceUnit.INCH));
+            telemetry.update();
+        }
+    }
+}
